c52375efbcf9baef8debafe82143687e3ad221c9
deryasd <derya.sdev@gmail.com>
2022-10-14 18:27:44 +0300

    hipotenusbulmada düzeltme yapıldı.

--- hipotenusbulma/src/com/company/Main.java
+++ hipotenusbulma/src/com/company/Main.java
@@ -4,18 +4,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int a,b;
-        double c;
+
+        int a,b,f;
+        double c,d,e;
 
         Scanner kenar = new Scanner(System.in);
         System.out.println("Birinci Kenarı Giriniz");
         a = kenar.nextInt();
         System.out.println("İkinci Kenarı Giriniz");
         b = kenar.nextInt();
+        System.out.println("Üçüncü Kenarı Giriniz");
+        f = kenar.nextInt();
 
+        //hipotenüs
         c = Math.sqrt((a*a)+(b*b));
-
         System.out.println("Üçgenin Hipotenüsü: "+ c);
+        //alan
+        d = a+b+f;
+        e = Math.sqrt( d * (d - a)* (d - b) * (d - f));
+        System.out.println("Üçgenin Alanı: "+ e);
+
+
     }
 }
