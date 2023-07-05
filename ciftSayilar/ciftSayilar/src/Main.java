00da7ea85edf11ba2a4d276309d7c99c07312409
deryasd <derya.sdev@gmail.com>
2023-07-05 21:51:02 +0300

    3 ve 4 ile bolunebilen sayilar olarak degistirildi.

--- ciftSayilar/ciftSayilar/src/Main.java
+++ ciftSayilar/ciftSayilar/src/Main.java
@@ -2,13 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int n;
+        int n,top = 0, sayi = 0;
+        double ort;
         System.out.print("Bir sayı giriniz:");
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
+        System.out.println("3 ve 4 ile tam bölünebilen sayılar:");
         for (int i = 0;i<=n;i++){
-            if(i%2 == 0)
-                System.out.print(i + " , ");
+            if(i%3 == 0 && i%4 == 0){
+                top +=i;
+                System.out.print(i + ",");
+                sayi++;}
         }
+        ort = top / sayi;
+        System.out.println("3 ve 4 ile bölünebilen sayıların ortalaması :"+ ort);
     }
 }
